e961ae509f1ac15e99e80e320d8168194358dbc5
heyjones <chris@heyjones.com>
2015-03-04 15:52:17 -0800

    rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Dpswmc2015::Application.routes.draw do
 
 	root 'users#new'
+	get '/rules' => 'pages#rules'
 	resources :users, only: [:new, :create, :index]
 	resources :entries, only: [:new, :create, :index, :show]
 	resources :notes, only: [:create]
